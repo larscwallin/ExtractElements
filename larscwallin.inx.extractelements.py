09c3a42888fb9fb242247c95896f63527aa6c2cb
Lars Wallin <larscwallin@gmail.com>
2013-10-08 17:13:34 +0200

    Update larscwallin.inx.extractelements.py
    Added optional javascript to support setting fill color for element id or entire svg doc.

--- larscwallin.inx.extractelements.py
+++ larscwallin.inx.extractelements.py
@@ -7,7 +7,8 @@ import string
 import webbrowser
 import threading
 import os.path
-
+import math
+import re
 sys.path.append('/usr/share/inkscape/extensions')
 
 import SvgDocument
@@ -22,12 +23,15 @@ class ExtractElements(inkex.Effect):
     exportTemplate = """<?xml version="1.0" standalone="no"?>
                     <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
                     "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100%" viewBox="0 0 {{element.width}} {{element.height}}" enable-background="new 0 0 {{element.width}} {{element.height}}" xml:space="preserve" preserveAspectRatio="xMinYMin">
+                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100%" viewBox="0 0 {{element.width}} {{element.height}}" xml:space="preserve" preserveAspectRatio="xMinYMin">
+                    <style/>
                     {{element.source}}
+                    {{js}}
                     </svg>"""
 
     cssTemplate = """.{{css.prefix}}{{element.label}}{{css.suffix}}:url({{element.source}});"""
     sassTemplate = """${{sass.var.prefix}}{{element.label}}{{sass.var.suffix}}="{{element.source}}";"""
+    js = """<script><![CDATA[(function(){if(document.location.hash){var a={},b;b=!1;location.href.split("?")[1].split("&").forEach(function(b){a[b.split("=")[0]]=b.split("=")[1]});a.attr="fill";(b=/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(a.value))&&a.attr&&a.value&&(b=a.id?document.getElementById(a.id):document.getElementsByTagName("svg")[0])&&b.setAttribute("style",a.attr+":"+a.value+";")}})();]]></script>"""
 
     def __init__(self):
         """
@@ -61,7 +65,9 @@ class ExtractElements(inkex.Effect):
               type = 'inkbool', dest = 'scour', default = False,
               help = 'Clean up drawing source using Scour?')
 
-
+        self.OptionParser.add_option('--includejs', action = 'store',
+              type = 'inkbool', dest = 'includejs', default = False,
+              help = 'Include Javascript to support ?color=hexvalue parameter?')
 
     def effect(self):
         """
@@ -74,6 +80,7 @@ class ExtractElements(inkex.Effect):
         self.reposition = self.options.reposition
         self.scour = self.options.scour
         self.renderSass = True
+        self.includeJS = self.options.includejs
         self.CSSSource = []
 
         self.getselected()
@@ -96,7 +103,6 @@ class ExtractElements(inkex.Effect):
             defs = '<defs/>'
         """
 
-
         # If no elements where selected we default to exporting every visible layer in the drawing
         if(self.selected.__len__() <= 0):
             self.selected = layers
@@ -109,20 +115,31 @@ class ExtractElements(inkex.Effect):
         if(self.selected.__len__() > 0):
             selected = []
 
-            # Iterate through all elements
+            # Iterate through all selected elements
             for element in self.selected:
 
                 elementLabel = str(element.get(inkex.addNS('label', 'inkscape'),''))
                 elementId = element.get('id')
 
+                if(elementLabel != ''):
+                    element.set('label',elementLabel)
+                    element.set('class',elementLabel)
+                else:
+                    pass
+
                 tagName= self.getTagName(element)
 
-                if(tagName== 'path'):
-
+                if(tagName == 'path'):
+                    # Paths can easily be moved by recalculating their d attributes
                     if(self.reposition or self.resizeDrawing):
                         pathData = self.movePath(element,0,0,'tl')
                         if(pathData):
                             element.set('d',pathData)
+
+                elif(tagName == 'g'):
+                    # Groups however are best "transformed" into place using translate
+                    # self.translateElement(element,0,0,False)
+                    pass
 
                 elementBox = list(simpletransform.computeBBox([element]))
                 elementBox[1] = (elementBox[1]-elementBox[0])
@@ -147,9 +164,12 @@ class ExtractElements(inkex.Effect):
                         tplResult = string.replace(self.exportTemplate,'{{element.width}}',str(self.svgWidth))
                         tplResult = string.replace(tplResult,'{{element.height}}',str(self.svgHeight))
 
-
                     #tplResult = string.replace(tplResult,'{{document.defs}}',defs)
                     tplResult = string.replace(tplResult,'{{element.source}}',elementSource)
+                    if(self.includeJS):
+                        tplResult = string.replace(tplResult,'{{js}}',self.js)
+                    else:
+                        tplResult = string.replace(tplResult,'{{js}}','')
 
                     if(self.scour):
                         tplResult = self.scourDoc(tplResult)
@@ -161,7 +181,7 @@ class ExtractElements(inkex.Effect):
                             'label':elementLabel,
                             'source':tplResult,
                             'box':elementBox
-                            })
+                        })
 
             for node in selected:
                 # Cache these in local vars
@@ -172,7 +192,6 @@ class ExtractElements(inkex.Effect):
                 if(content!=''):
 
                     if(self.base64Encode):
-
 
                         if(self.renderSass):
                             content = ('$data-url-'+label+':"data:image/svg+xml;name='+label+';base64,'+(base64.b64encode(content))+'";')
@@ -232,6 +251,165 @@ class ExtractElements(inkex.Effect):
             tagName= str(type)
 
         return tagName
+
+    # Move element using transform translate
+    def translateElement(self,node,x,y,relative = False):
+
+        # Grab transform attribute if it exists.
+        transform = node.get('transform','')
+
+        # Compute the nodes bounding box
+        box =list(simpletransform.computeBBox([node]))
+
+        pos_x = box[0]
+        pos_y = box[2]
+
+        # rotation center is not a breeze to calculate from the matrix, so thanks inkscape ;)
+        origin_x = float(node.get(inkex.addNS('transform-center-x', 'inkscape'),0))
+        origin_y = float(node.get(inkex.addNS('transform-center-y', 'inkscape'),0))
+        origin_x = origin_x + ( box[1] / 2)
+        origin_y = (origin_y * -1) + ( box[3] / 2)
+
+        if(transform==''):
+            # If there is no transform attribute on the node we add one
+            node.attrib['transform'] = ''
+
+        # simpletransform returns a multi dim array of matrix values
+        transform = simpletransform.parseTransform(transform)
+
+        transformObject = self.normalizeMatrix(transform)
+        inkex.debug(transformObject)
+        #offset_x = (transform[0][2]-pos_x)
+        #offset_y = (transform[1][2]-pos_y)
+        offset_x = (pos_x * -1)
+        offset_y = (pos_y * -1)
+
+        inkex.debug([offset_x,offset_y])
+
+        transform = simpletransform.parseTransform(('translate(' + str(offset_x) + ' ' + str(offset_y) + ')'),transform)
+
+        transformObject = self.normalizeMatrix(transform)
+        inkex.debug(transformObject)
+
+        inkex.debug(transform)
+
+        if(relative == False):
+            matrix = simpletransform.formatTransform(transform)
+            node.set('transform',matrix)
+            inkex.debug(matrix)
+        else:
+            simpletransform.applyTransformToNode(transform,node)
+
+
+    def parseStyleAttribute(self,str):
+
+        #inkex.debug(self.debug_tab + 'Got style ' + str)
+
+        rules = str.split(';')
+        parsed_set = {}
+        result = ''
+        for rule in rules:
+            parts = rule.split(':')
+
+            if(len(parts) > 1):
+
+                key = self.camelConvert(parts[0])
+                val = self.camelConvert(parts[1])
+
+                if(key== 'filter'):
+                    parsed_set['filter'] = self.parseFilter(val)
+                elif(key == 'fill' and val.find('url(#') > -1):
+                    parsed_set['fillGradient'] = self.parseGradient(val)
+                elif(key == 'stroke' and val.find('url(#') > -1):
+                    parsed_set['strokeGradient'] = self.parseGradient(val)
+                else:
+                    parsed_set[key] = val
+
+        return parsed_set
+
+    def expandMatrix(self,normalizedMatrix):
+        pass
+
+    def normalizeMatrix(self,matrix):
+      degree = 180 / math.pi
+      radian = math.pi / 180
+
+      a = matrix[0][0]
+      b = matrix[1][0]
+      c = matrix[0][1]
+      d = matrix[1][1]
+      tx = matrix[0][2]
+      ty = matrix[1][2]
+
+      scaleX = math.sqrt((a * a) + (c * c))
+      scaleY = math.sqrt((b * b) + (d * d))
+
+      sign = math.atan(-c / a)
+      rad  = math.acos(a / scaleX)
+      deg  = rad * degree
+      reflectX = (a < 0)
+      reflectY = (d < 0)
+
+      if (deg > 90 and sign > 0):
+        rotation = (360 - deg) * radian
+
+      elif (deg < 90 and sign < 0):
+        rotation = (360 - deg) * radian
+      else:
+        rotation = rad
+
+      rotationInDegree = rotation * degree
+
+      # If we have a reflected matrix we subtract 180 degrees
+      if(reflectX or reflectY):
+        rotationInDegree = (rotationInDegree - 180)
+        rotation = (rotation - math.pi)
+
+      if(reflectX):
+        scaleX = (scaleX * -1)
+      if(reflectY):
+        scaleY = (scaleY * -1)
+
+
+      return {
+        'scale':{
+          'x':scaleX,
+          'y':scaleY
+        },
+        'rotate':{
+          'degree':rotationInDegree,
+          'radiance':rotation
+        },
+        'reflect':{
+            'x':str(reflectX),
+            'y':str(reflectY)
+        },
+        'translate':{
+          'x':tx,
+          'y':ty
+        },
+        'matrix':matrix
+    }
+
+    def matrixToList(self, matrix):
+        """
+        From matrix order,
+
+        1          3           5         2          4          6
+
+        to  sequencial list
+
+        1          2           3          4          5         6
+        """
+
+        return [
+            matrix[0][0],
+            matrix[1][0],
+            matrix[0][1],
+            matrix[1][1],
+            matrix[0][2],
+            matrix[1][2]
+        ]
 
 
     def movePath(self,node,x,y,origin):
