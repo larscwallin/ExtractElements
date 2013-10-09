44e7afd83b8cd191955416f36e656279ffc2b23d
Lars Wallin <larscwallin@gmail.com>
2013-10-09 16:29:00 +0200

    Update larscwallin.inx.extractelements.js
    Element Id is now set using url fragment (#theid) instead of querystring parameter.

--- larscwallin.inx.extractelements.js
+++ larscwallin.inx.extractelements.js
@@ -1,6 +1,10 @@
 (function(){
-    if(document.location.hash !== undefined){
+	// Check so that running the script is really necessary
+    if( document.location.href.indexOf( '?' ) > 0 ){
 	    var params = [];
+	    var rawParams = [];
+	    var hashes = [];
+	    var href = '';
 	    var element;
 		var elementId;
 	    var rules = '';
@@ -15,60 +19,90 @@
 	    	'stroke-width'
 	    ];
 
-	    location.href.split( '?' )[1].split( '&' ).forEach(
-	        function( i )
-	        {
-	        	var rule = i.split( '=' );
-	        	var key = rule[0];
-	        	var val = rule[1];
-	        	if(key!=='id'){
-		        	if(validRules.indexOf(key)>=0){
-		        		hasRules = true;
-		            	params.push(rule);
-		            }else{
-		            	// Not a valid rule
-		            }
-	        	}else{
-	        		// Save the id, if it exists.
-	        		elementId = val;
-	        	}
-	        }
-	    );
+	    href = location.href;
 
-	    if( hasRules )
-	    {
+	    if( document.location.hash !== '' ){
+	    	// The hashes as the ids that should be affected by the script
+			hashes = document.location.href.split( '#' );
 
-	        if(!elementId){
-	        	// Got no element id so we apply the style to the svg doc
-	        	element = document.getElementsByTagName('svg')[0];
-	        }else{
-	        	element = document.getElementById( elementId );
-	        }
+			if( hashes.length > 0 ){
+				// For now we only allow one id
+				elementId = hashes[1];
+				// Now we remove the hashes from the URL
+				href = hashes[0];
 
-	        if(element){
-				
-				params.forEach(
-			        function( rule )
+			}else{
+				elementId = false;
+			}
+
+	    }else{
+	    	// No hash elements in the URL
+    	}
+
+    	href = href.split( '?' );
+
+    	if( href.length > 1 ){
+
+		    rawParams = href[1].split( '&' );
+		    
+		    if( rawParams.length > 0 ){
+			    
+			    rawParams.forEach(
+		
+			        function( i )
 			        {
-			        	var val;
-			        	if(rule.length === 2){
-				        	val  = rule[1];
-							rules += ( rule[0] + ':' + val + ';');
-						}else{
-							// Not a key/val pair								
-						}
-			        }
+			        	var rule = i.split( '=' );
+			        	var key = rule[0];
+			        	var val = rule[1];
+			        	if( validRules.indexOf( key ) >=0 ){
+			        		hasRules = true;
+			            	params.push( rule );
+			            }else{
+			            	// Not a valid rule
+			            }		        
+			         }
+		
 			    );
-
-				if(rules != ''){
-					element.setAttribute( 'style', rules );
-				}else{
-					// No rules where returned from the foreach loop.
-				}
-
-	        }else{
-	        	// element id reference returned null
+			
+			}else{
+				// No parameters in the URL 	
 	        }
-	    }
+
+		    if( hasRules )
+		    {
+
+		        if( !elementId ){
+		        	// Got no element id so we apply the style to the svg doc
+		        	element = document.getElementsByTagName( 'svg' )[0];
+		        }else{
+		        	element = document.getElementById( elementId );
+		        }
+
+		        if(element){
+					
+					params.forEach(
+				        function( rule )
+				        {
+				        	var val;
+				        	if( rule.length === 2 ){
+					        	val = rule[1];
+								rules += ( rule[0] + ':' + val + ';' );
+							}else{
+								// Not a key/val pair								
+							}
+				        }
+				    );
+
+					if( rules != '' ){
+						element.setAttribute( 'style', rules );
+					}else{
+						// No rules where returned from the foreach loop.
+					}
+
+		        }else{
+		        	// element id reference returned null
+		        }
+		    }
+		}
 	}
 })();
